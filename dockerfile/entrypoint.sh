d920c4147f698e56daf555e6e1b6598abd79711a
Truong Pham <xmars4@users.noreply.github.com>
2023-10-20 16:56:54 +0700

    Update entrypoint.sh

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -v PASSWORD_FILE ]; then
-    PASSWORD="$(< $PASSWORD_FILE)"
+    PASSWORD="$(<$PASSWORD_FILE)"
 fi
 
 # set the postgres database host, port, user and password according to the environment
@@ -17,9 +17,9 @@ DB_ARGS=()
 function check_config() {
     param="$1"
     value="$2"
-    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
-        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
-    fi;
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC"; then
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" | cut -d " " -f3 | sed 's/["\n\r]//g')
+    fi
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
@@ -32,28 +32,29 @@ check_config "db_password" "$PASSWORD"
 COMMAND_PARAM_NAME="command"
 COMMAND=
 function get_additional_command() {
-    if grep -q -E "^\s*\b${COMMAND_PARAM_NAME}\b\s*=" "$ODOO_RC" ; then
+    if grep -q -E "^\s*\b${COMMAND_PARAM_NAME}\b\s*=" "$ODOO_RC"; then
         COMMAND=$(grep -E "^\s*\b${COMMAND_PARAM_NAME}\b\s*=" "$ODOO_RC" | cut -d "=" -f2 | sed 's/[\n\r]//g')
-    fi;
+    fi
 }
 get_additional_command
 
 case "$1" in
-    -- | odoo)
-        shift
-        if [[ "$1" == "scaffold" ]] ; then
-            exec odoo "$@"
-        else
-            wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-            exec odoo "$@" "${DB_ARGS[@]}" $COMMAND
-        fi
-        ;;
-    -*)
+-- | odoo)
+    shift
+    if [[ "$1" == "scaffold" ]]; then
+        exec odoo "$@"
+    else
         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
         exec odoo "$@" "${DB_ARGS[@]}" $COMMAND
-        ;;
-    *)
-        exec "$@"
+    fi
+    ;;
+-*)
+    wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+    exec odoo "$@" "${DB_ARGS[@]}" $COMMAND
+    ;;
+*)
+    exec "$@"
+    ;;
 esac
 
 exit 1
